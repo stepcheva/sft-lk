3849858548f26e837b65d72f6450765074252d16
stzv78 <stzv78@mail.ru>
2018-06-14 16:22:01 +0400

    add passwordDayIsValid() to model for view in users profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,20 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Model
 {
+    use Notifiable;
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    public $counter_id;
 
     protected $fillable = [
         'lastName',
@@ -20,6 +24,10 @@ class User extends Model
         'email',
         'phone',
         'password',
+
+    ];
+
+    protected $hidden = [
         'remember_token',
     ];
 
@@ -28,5 +36,46 @@ class User extends Model
         return $this->hasOne('App\Models\Applicator');
     }
 
+    public function setPasswordUntil()
+    {
+        return $this->passwordUntil = Carbon::now()->addDays(25)->format('Y-m-d');
+    }
+
+    public function getPasswordUntil()
+    {
+        return $this->passwordUntil;
+    }
+
+    public function passwordIsValid()
+    {
+        return (Carbon::now() <= $this->getPasswordUntil()) or false;
+    }
+
+    public function passwordDayIsValid()
+    {
+        $date = Carbon::parse($this->getPasswordUntil());
+        return $date->diffInDays(Carbon::now());
+    }
+
+    public static function check()
+    {
+        return Session::get('user');
+    }
+
+    public static function auth($email, $password)
+    {
+        $user = self::where(['email' => $email, 'password' => $password])->first();
+        dd($user);
+        if (!is_null($user) && $user->passwordIsValid()) {
+            Session::put('user', true);
+            return $user;
+        }
+        return false;
+    }
+
+    public static function logout()
+    {
+        Session::forget('user');
+    }
 }
 
