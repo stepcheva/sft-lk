356bac142e94d4e2c040248af575313052b2d25f
stzv78 <stzv78@mail.ru>
2018-06-15 12:16:10 +0400

    added :active in classes for items

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,22 +1,24 @@
 <li class="b-topnav__item">
-    <a href="{{ route('applications.index', ['applicator_id' => $user->applicator->id]) }}" class="b-topnav__link active">Мои заявки</a>
+    <a href="{{ route('applications.index', ['applicator_id' => $user->applicator->id]) }}" class="
+    {{ in_array(Route::currentRouteName(), ['applications.index', 'applications.create']) ? 'b-topnav__link active': 'b-topnav__link' }}">Мои заявки</a>
 </li>
 <li class="b-topnav__item">
-    <a href="{{ route('productranges.list', ['applicator_id' => $user->applicator->id]) }}" class="b-topnav__link ">Номенклатура</a>
+    <a href="{{ route('productranges.list', ['applicator_id' => $user->applicator->id]) }}" class="
+    {{ in_array(Route::currentRouteName(), ['productranges.list']) ? 'b-topnav__link active': 'b-topnav__link' }}">Номенклатура</a>
 </li>
 <li class="b-topnav__item">
     <a href="" class="b-topnav__link ">Документация</a>
 </li>
 <li class="b-topnav__item">
-    <a href="{{ route('contactquery.index', ['applicator_id' => $user->applicator->id]) }}" class="b-topnav__link ">Мои обращения</a>
+    <a href="{{ route('contactquery.index', ['applicator_id' => $user->applicator->id]) }}" class="
+    {{ in_array(Route::currentRouteName(), ['contactquery.index']) ? 'b-topnav__link active': 'b-topnav__link' }}">Мои обращения</a>
 </li>
 <li class="b-topnav__item">
     <a href="" class="b-topnav__link b-topnav__link-toggle js-nav-toggle">{{ $user->firstName ." ". $user->lastName }}</a>
     <ul class="b-submenu__list js-nav">
         <li class="b-submenu__item ">
-            <a class="b-submenu__link " href="{{  route('applicators.show', ['applicator_id' => $user->applicator->id]) }}">
-                Мой профиль
-            </a>
+            <a  href="{{  route('applicators.show', ['applicator_id' => $user->applicator->id]) }}" class="
+                {{ in_array(Route::currentRouteName(), ['applicators.show']) ? 'b-topnav__link active': 'b-topnav__link' }}">Мой профиль</a>
         </li>
         <li class="b-submenu__item ">
             <a class="b-submenu__link " href="{{ route('logout') }}"
