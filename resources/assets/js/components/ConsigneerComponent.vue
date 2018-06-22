46c47235d9d26947215e56bf29146004b91b6052
stzv78 <stzv78@mail.ru>
2018-06-22 16:15:26 +0400

    add ConsigneerComponent for productranges.list template

---
+++ resources/assets/js/components/ConsigneerComponent.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+    <div class="receiver-title">Выберите грузополучателя</div>
+          <select class="js-default-select form__select-tiny">{{ applicator }}
+                 <option value="receiver-01">ОАО «Константинопольский и ко»</option>
+                                       <option value="receiver-02">ОАО «Иванов и ко»</option>
+                                       <option value="receiver-03">ОАО «Петров и ко»</option>
+                                   </select>
+                                   <div class="nm-address">
+                                       249000, Ульяновская обл., г.Ульяновск,
+                                       ул.Дмитрия Ульянова, д.192
+                                   </div>
+                                   <div class="flex banking">
+                                       <div class="banking__item">
+                                           <span class="banking__title">ИНН:</span>
+                                           4003014954
+                                       </div>
+                                       <div class="banking__item">
+                                           <span class="banking__title">КПП:</span>
+                                           40030001
+                                       </div>
+                                   </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'applicator'
+        ],
+
+        created() {
+            this.update()
+        },
+
+        methods: {
+            update() {
+                console.log(this.applicator);
+            }
+        }
+    }
+</script>
--- resources/views/templates/productranges/list.blade.php
+++ resources/views/templates/productranges/list.blade.php
@@ -1,55 +1,103 @@
-@extends('layouts._app')
+@extends('layouts.master')
+
+@section('title', "Номенклатура")
 
 @section('content')
-    <div class="container">
 
-        @include ('flashes')
-        <div class="row">
-            <div class="col-md-12">
-                <div class="panel panel-default">
-                    <div>
+    @include('flashes')
 
+    <div class="nm-header cntr">
+        <h1 class="nm-title">Номенклатура</h1>
+        <p class="nm-subtitle">Данная номенклатура является текущей из справочника по условиям сотрудничества</p>
+    </div>
+    <div class="nm-body">
+        <div class="a-title">
+            <div class="a-title__h3">Информация по номенклатуре</div>
+        </div>
+        <div class="nm-info">
+            <div class="flex info-flex">
+                <div class="flex__col info-flex__col">
+                    <div class="info-title-small">Время отсрочки</div>
+                    <span class="info-line">{{ $applicator->cooperation->delayed_payment }}</span>
+                </div>
+                <div class="flex__col info-flex__col">
+                    <div class="info-title-small">Время действия соглашения</div>
+                    <span class="info-line">до {{ $applicator->cooperation->contract_period }}</span>
+                </div>
+                <div class="flex__col info-flex__col">
+                    <div class="info-title-small">Объем на месяц</div>
+                    <span class="info-line">{{ $applicator->cooperation->monthly_max_volume }} тонн</span>
+                </div>
+                <div class="flex__col info-flex__col">
+                    <div class="info-title-small">Бонус</div>
+                    <span class="info-line">{{ $applicator->cooperation->bonus }} %</span>
+                </div>
+            </div>
+        </div>
+        <div class="nm-receivers">
+            <div class="a-title">
+                <div class="a-title__h3">Данные по грузополучателям</div>
+            </div>
+            <div class="flex receivers">
+                <div class="receivers__col receivers__col-left">{{ $applicator->id }}
+                    <!--ConsineerComponent -->
+                    <consigneer-component :applicator="{{ $applicator->id }}"></consigneer-component>
+                    <!--EndComponent-->
+                    <div class="info-line-small">Диаметр рулона: <span class="info-line-small__color">1300</span></div>
+                    <div class="info-line-small">Диаметр гильзы: <span class="info-line-small__color">100</span></div>
+                </div>
+                <div class="receivers__col receivers__col-right">
+                    @if (isset($productranges))
+                    <div class="infoblocks">
+                        @foreach($productranges as $product)
+                        <div class="infoblock @if($loop->last) infoblock_last @endif flex">
+                            <div class="infoblock__col">
+                                <div class="info-title infoblock__info-title">Данные о грузе</div>
+                                <div class="info-line-small">Марка: <span class="info-line-small__color">{{ $product->brand }}</span></div>
+                                <div class="info-line-small">Граммаж: <span class="info-line-small__color">{{ $product->grammage }}</span></div>
+                                <div class="info-line-small">Формат: <span class="info-line-small__color">{{ $product->format }}</span></div>
+                            </div>
+                            <div class="infoblock__col">
+                                <div class="info-title infoblock__info-title">Объём продукции</div>
+                                <div class="info-line-small">Минимальная партия: <span class="info-line-small__color">{{ $product->min_lot }} т</span></div>
+                                <!--<div class="info-line-small">Объём на месяц: <span class="info-line-small__color">50 т</span></div>-->
+                            </div>
+                            <div class="infoblock__col">
+                                <div class="info-title infoblock__info-title">Цена</div>
+                                <!--Изменить согласно видам доставки-->
+                                <div class="info-line-small">Самовывоз: <span class="info-line-small__color"> 12 100 <span class="rouble">q</span></span></div>
+                                <div class="info-line-small">Авто: <span class="info-line-small__color">19 600 <span class="rouble">q</span></span></div>
+                                <div class="info-line-small">Ж/Д: <span class="info-line-small__color">15 600 <span class="rouble">q</span></span></div>
+                            </div>
+                               <!--Убираем примечание
+                                <div class="infoblock__col infoblock__col_last">
+
+                                    <div class="receiver-toggler js-toggler">
+                                        <div class="info-title infoblock__info-title">Примечание</div>
+                                        <div class="receiver-toggler__txt js-toggler-text">
+                                            <div class="js-toggler-inner">
+                                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem error sit accusantium doloremque error Sed ut perspiciatis unde omnis iste natus error sit voluptatem error sit accusantium doloremque error Sed ut perspiciatis unde omnis iste natus error sit voluptatem error sit accusantium doloremque error
+                                            </div>
+                                        </div>
+                                        <div class="receiver-toggler__btn js-toggler-btn">
+                                            <span class="js-show">Показать полностью</span>
+                                            <span class="js-hide">Скрыть</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                -->
+                        </div>
+                        @endforeach
                     </div>
-                    <div class="panel-heading"><h3>Номенклатура</h3></div>
-                    <table id="productranges" class="table table-bordered table-striped">
-                        <thead>
-                        <tr>
-                            <th class="text-center">Марка</th>
-                            <th class="text-center">Граммаж</th>
-                            <th class="text-center">Формат</th>
-                            <th class="text-center">Минимальный объем</th>
-
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                        </thead>
-                        <tbody>
-
-
-                        @if (isset($productranges))
-
-                            @foreach($productranges as $key => $product)
-                                <tr>
-                                    <td> {{ $product->brand }} </td>
-                                    <td> {{ $product->grammage }} </td>
-                                    <td> {{ $product->format }} </td>
-                                    <td> {{ $product->min_lot }} </td>
-
-                                </tr>
-                            @endforeach
-                            <tr>
-
-                            </tr>
-                        @else
-                            <h2> Нет товаров у текущего поставщика</h2>
-                        @endif
-                        </tbody>
-                    </table>
-
+                    @else
+                        <h2> Нет товаров у текущего поставщика</h2>
+                    @endif
                 </div>
             </div>
         </div>
     </div>
-
 @endsection
+
+@push('script')
+    <script src="{{ asset('js/app.js') }}"></script>
+@endpush
