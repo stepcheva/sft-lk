ed25180388adbea515cdc3ffa17417dcf6c710e3
stzv78 <stzv78@mail.ru>
2018-06-25 16:39:25 +0400

    add Unit model

---
+++ app/Models/Unit.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Unit extends Model
+{
+    protected $fillable = [
+        'decada',
+        'volume',
+        'price',
+        'productrange_id',
+        'application_id',
+        'lunit_id',
+    ];
+
+    public function application()
+    {
+        return $this->belongsTo('App\Models\Application');
+    }
+
+    public function productrange()
+    {
+        return $this->belongsTo('App\Models\Productrange');
+    }
+
+    public function lunit()
+    {
+        return $this->belongsTo('App\Models\Lunit');
+    }
+}
