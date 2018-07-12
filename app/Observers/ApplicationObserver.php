69be1c0685bce28dd0c226ac3062bcfeb9d67a05
stzv78 <stzv78@mail.ru>
2018-07-12 11:02:08 +0400

    add observer to change Appliations version in its number

--- app/Observers/ApplicationObserver.php
+++ app/Observers/ApplicationObserver.php
@@ -3,19 +3,24 @@ namespace App\Observers;
 
 use App\Models\Application;
 use App\Models\Admin;
+use App\Models\Unit;
 
+class ApplicationObserver
 
-public function updated(Application $application)
 {
-    $changes = array_diff($application->getOriginal(), $application->getAttributes());
 
-    if (!empty($changes)) {
-        if (Admin::check()) {
-            $application->setPrefixNumber('admin');
-        } else {
-             return $application->user->id === Auth::user()->id;
+    public function updated(Application $application)
+    {
+        $changes = array_diff($application->getOriginal(), $application->getAttributes());
+
+        if (!empty($changes)) {
+            if (Admin::check()) {
+                $application->setPrefixNumber('admin');
+            } else {
+                 return $application->user->id === Auth::user()->id;
+                }
             }
-        }
 
-    return $changes or false;
+        return $changes or false;
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Application;
 use App\Models\User;
 use App\Observers\UsersObserver;
 use Illuminate\Support\ServiceProvider;
@@ -16,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         User::observe(UsersObserver::class);
+        Application::observe(ApplicationObserver::class);
     }
 
     /**
